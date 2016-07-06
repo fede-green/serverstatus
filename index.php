83593f1f2229f14da56d9ef58a125de1ed191a0e
federico prato <federico.prato@wunderkraut.com>
2016-07-06 13:22:57 +0300

    First commit

---
+++ index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Care Status</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/style.css"/>
+  </head>
+  <body role="document">
+<?php
+require_once 'src/uptimerobot.php';
+require_once 'conf/config.php';
+$upRobot = new UptimeRobot();
+
+$upRobot::configure($api, 1);
+
+$upRobot->setFormat('json'); //Define the format of responses (json or xml)
+
+/**
+ * Get monitors
+ */
+$monitors = $upRobot->getMonitors(); //Get all monitors
+$upservers = 0;
+$downservers = 0;
+$pausedservers = 0;
+$paused_what = array();
+$down_what = array();
+foreach($monitors->monitors as $monitors_arr){
+  foreach($monitors_arr as $monitor){
+    switch ($monitor->status) {
+      case '2':
+        $upservers ++;
+        break;
+      case '0':
+        $pausedservers ++;
+        $paused_what[] = $monitor;
+        break;
+      case '8':
+        $down_what[] = $monitor;
+        $downservers ++;
+      break;
+      case '9':
+        $down_what[] = $monitor;
+        $downservers ++;
+      break;
+    }
+  }
+}
+?>
+<div role="main" class="container theme-showcase">
+  <div class="page-header">
+  <h1>Care Status</h1>
+  </div>
+  <h2>
+  <span class='label label-success'><?= $upservers ?> servers up</span>
+  <span class='label label-warning'><?= $pausedservers ?> servers paused</span>
+  <span class='label label-danger'><?= $downservers ?> servers down</span>
+  </h2>
+  <div class="row">
+  <?php
+  if($pausedservers > 0){
+    foreach($paused_what as $monitor){
+      ?>
+      <div class="panel-warning">
+        <h3 class='panel-title'><?= $monitor->friendlyname ?></h3>
+        <div class='panel-body'><?= $monitor->url ?></div>
+      </div>
+      <?php
+    }
+  }
+  ?>
+  <?php
+  if($downservers > 0){
+    foreach($down_what as $monitor){
+      ?>
+      <div class="panel-danger">
+        <h3 class='panel-title'><?= $monitor->friendlyname ?></h3>
+        <div class='panel-body'><?= $monitor->url ?></div>
+      </div>
+      <?php
+    }
+  }
+  ?>
+  </div>
+</div>
+<footer class="footer">
+  <div class="container">
+    <p>
+      Dashboard recap of Wunderkraut's Uptimerobot monitors. Done with <a href="https://github.com/watchfulli/uptimeRobot">uptimeRobot</a> and using <a href="http://getbootstrap">Bootstrap</a>.
+    </p>
+  </div>
+</footer>
+</body>
